cb652d85be6de9f4590650034bffbd84bb171a52
KRAZJUST <skalkadavid03@gmail.com>
2023-03-27 12:36:20 +0200

    Created basic page and working timer.

---
+++ dynamic.js
@@ -0,0 +1,32 @@
+
+var count = 60 * 1000;
+var timer;
+var timerRunning = false;
+
+function startTimer(){
+  if(!timerRunning){
+    timerRunning = true;
+    timer = setInterval(function(){
+      count -= 10;
+      var minutes = Math.floor((count / 1000 / 60) % 60).toString().padStart(2, '0');
+      var seconds = Math.floor((count / 1000) % 60).toString().padStart(2, '0');
+      var milliseconds = Math.floor(count % 1000).toString().padStart(3, '0');
+      document.getElementById("timer").innerHTML = `${minutes}:${seconds}:${milliseconds}`;
+
+      if(count <= 0){
+        clearInterval(timer);
+        alert("Time's up!");
+        timerRunning = false;
+      }
+    }, 10);
+  }
+}
+
+function stopTimer(){
+  clearInterval(timer);
+  count = 60 * 1000;
+  document.getElementById("timer").innerHTML = "01:00:00";
+  timerRunning = false;
+}
+
+document.addEventListener("keydown", startTimer);
